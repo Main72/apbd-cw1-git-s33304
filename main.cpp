1b58e39a6b1c35e1219c551159b92ff58efebdeb
Arsenii Marushchak <s33304@pjwstk.edu.pl>
2026-03-12 20:01:17 +0100

    add function Max (from feature-max)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 void CalculateAverage(int value) {
     std::cout << value + 1 << "!\n";
 }
+void CalculateMax(int value) {
+    std::cout << value + 100 << "!\n";
+}
 
 int main() {
     auto lang = "C++";
@@ -16,6 +19,7 @@ int main() {
     std::cout << "Hello GitHub???" << lang << "!\n";
     std::cout << "Hello GitHub===" << lang << "!\n";
     CalculateAverage(2);
+    CalculateMax(1);
 
     return 0;
 }
